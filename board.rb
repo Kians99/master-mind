e41876bb42ed870bb9e3d6de68cfaf5feeb685bf
Kian <Ksadeghi@sas.upenn.edu>
2020-08-02 20:04:54 -0400

    Most cases correctly loops through game

--- board.rb
+++ board.rb
@@ -1,15 +1,35 @@
 #computer class
 class Board
-  COLORS = %w[Yellow White Black Brown Orange Red Green Blue].freeze
+  COLORS = %w[yellow white black brown orange red green blue].freeze
 
   def initialize
     @code = Array.new(0)
     4.times { @code.push(COLORS.sample) }
-    
   end 
 
-  def is_game_over?
-    false
+  attr_reader :code
+  
+  def contain_color?(input_cols)
+    count = 0
+    input_cols.each do |col|
+      if code.include?(col)
+        count += 1
+      end
+    end
+    count
+  end
 
+  def iden_col_and_pos(input_cols)
+    count = 0
+    for i in 0..input_cols.length - 1
+      if code[i] == input_cols[i]
+        count += 1
+      end
+    end
+    count
+  end
+
+  def game_over?(input_cols)
+    input_cols == code
   end
 end
